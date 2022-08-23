1b0785da91aba3ecedc54802f57c5d49b0524791
Esteban <45163227+Zoumbar@users.noreply.github.com>
2022-08-23 11:40:34 +0200

    mailto

--- frontend-react/src/container/Footer/Footer.jsx
+++ frontend-react/src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="esteban.ouvre.pro@gmail.com" className="p-text">esteban.ouvre.pro@gmail.com</a>
+          <a href="mailto:esteban.ouvre.pro@gmail.com" className="p-text">esteban.ouvre.pro@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
