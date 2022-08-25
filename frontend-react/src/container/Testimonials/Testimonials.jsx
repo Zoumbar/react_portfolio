d9d8db6f6c56cd7d6f28c812d0b383d76e8f653e
Esteban <45163227+Zoumbar@users.noreply.github.com>
2022-08-25 11:54:08 +0200

    id testimonials

--- frontend-react/src/container/Testimonials/Testimonials.jsx
+++ frontend-react/src/container/Testimonials/Testimonials.jsx
@@ -32,7 +32,7 @@ const Testimonial = () => {
     <>
       {testimonials.length && (
         <>
-          <div className="app__testimonial-item app__flex">
+          <div className="app__testimonial-item app__flex" id='testimonials'>
             <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
             <div className="app__testimonial-content">
               <p className="p-text">{testimonials[currentIndex].feedback}</p>
