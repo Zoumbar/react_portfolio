ba65d2701a72be23da2071f332bf35dd82778bcb
Esteban <esteban.ouvre@gmail.com>
2023-02-20 20:44:04 +0100

    Refacto

--- backend_sanity/schemas/experiences.js
+++ backend_sanity/schemas/experiences.js
@@ -9,6 +9,16 @@ export default {
       type: "string",
     },
     {
+      name: "beginning",
+      title: "beginning",
+      type: "string",
+    },
+    {
+      name: "end",
+      title: "End",
+      type: "string",
+    },
+    {
       name: "works",
       title: "Works",
       type: "array",
